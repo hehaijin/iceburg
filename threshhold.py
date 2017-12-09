23cc0768d15042c71de18cf3786c9eec212593cb
hehaijin <hehaijin1981@gmail.com>
2017-12-08 23:34:18 -0700

    threshhold

---
+++ threshhold.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import os.path as path
+import os
+
+
+filename='submission.csv'
+#outputfilename='submission2.csv'
+
+datapath=path.join(os.getcwd(),filename)
+
+submitfile=open(datapath)
+
+df=pd.read_csv(submitfile)
+
+for i, row in df.iterrows():
+	if row["is_iceberg"] >= 0.5:
+		df.set_value(i,'is_iceberg',0.9)
+	else:
+		df.set_value(i,'is_iceberg',0.1)
+		
+df.to_csv("submission2.csv",index=False)
+		
+
+
