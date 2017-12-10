c4f81e74d295f15d5ba43aaa1dce7f573bb96e9d
hehaijin1981 <hehaijin1981@gmail.com>
2017-12-10 03:23:48 +0000

    export working

--- export.py
+++ export.py
@@ -1,10 +1,13 @@
 import cv2
 import json
 import pandas as pd
+import os
+import os.path as path
+import numpy as np
 
-
-datapath='.\data\processed'
-with open(path.join(datapath, 'train.json')) as trainfile:
+#datapath='.\data\processed'
+outputpath='pics'
+with open(path.join("data","processed", 'train.json')) as trainfile:
 	data=pd.read_json(trainfile)
 	gin = np.asarray([np.asarray(p).reshape(75,75) for p in data['band_1']])
 	gin = gin.reshape(1604,75,75,1)
@@ -12,5 +15,10 @@ with open(path.join(datapath, 'train.json')) as trainfile:
 	gin2 = gin2.reshape(1604,75,75,1)
 	for i in range(1604):
 		t=data.iloc[i]['is_iceberg']
-		cv2.imwrite("band1-"+str(i)+t+".jpg",gin[i,:,:,:])
-		cv2.imwrite("band2-"+str(i)+t+".jpg",gin[i,:,:,:])
+		img= gin[i,:,:,0]
+		img=(img-img.min())/(img.max()-img.min())
+		#print(img)
+		img2=gin2[i,:,:,0]
+		img2=(img2-img2.min())/(img2.max()-img2.min())
+		cv2.imwrite(path.join(outputpath,"band1-"+str(i)+"-"+str(t)+".jpg"),img)
+		cv2.imwrite(path.join(outputpath,"band2-"+str(i)+"-"+str(t)+".jpg"),img2)
