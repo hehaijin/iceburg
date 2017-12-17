57fe6e661f95bbb475c5cf00994451ff58afb7ea
hehaijin <hehaijin1981@gmail.com>
2017-12-16 21:46:44 -0700

    validate

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import sys
-
+import numpy as np
 
 #print(sys.version_info)
 print("Installing XXX...      ", end="")
 print("[DONE]")
+np.random.seed(666)
+
+print(np.random.random(10))
+print(np.random.random(10))
+print(np.random.random(10))
